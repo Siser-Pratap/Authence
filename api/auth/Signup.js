ea72baeb3412d60b5a956dac0b25dcd8a85a3937
Siser Pratap <siserinsevoc@gmail.com>
2024-10-09 02:08:33 +0530

    added mongoose connection

--- api/auth/Signup.js
+++ api/auth/Signup.js
@@ -1,31 +1,37 @@
 import bcryptjs from "bcryptjs";
 import user_black_one from "../../userSchema/userSchema.js";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
 
-dotenv.config();
+// dotenv.config({path:"./.env"});
 
 const signup = async(req, res)=>{
+
+    
     
     console.log(req.body);
+   
+
     const dbUrl = req.body.dbUrl;
     if(!dbUrl){
         const {username, password, email} = req.body;
-        mongoose.connect(`${process.env.MONGODB_URL}`)
-        .then(()=>console.log("Connected to database"))
-        .catch((err)=>console.log(err));
+        await mongoose.connect("mongodb+srv://jaysiserpratap:siser123@cluster0.bs31t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+            .then(()=>console.log("Connected to database"))
+            .catch((err)=>console.log(err));
         const hashedPassword =bcryptjs.hashSync(password, 10);
         console.log(hashedPassword);
         const newUser = new user_black_one({username, password:hashedPassword, email});
+        try {
+            await newUser.save();
+            console.log("user Created");
+            res.status(201).json({message:"User created Successfully", data:{username, email, password}});
+        } catch (error) {
+            console.log(error.message);
+            res.status(401).json({message:"Error creating user"});
+        }
     }
     
-    // const dbUrl = req.body.dbUrl;
-    
-    // if(!dbUrl){
-        
-    // }
-    
-    
-
+    console.log("hello");
     // const userName = await user_black_one.findOne({username: username});
     // const Email = await user_black_one.findOne({email: email});
 
@@ -36,15 +42,7 @@ const signup = async(req, res)=>{
     // if(Email){
     //     res.json({message:"User already registered"});
     // }
-
     
-
-    try {
-        await newUser.save();
-        res.status(201).json({message:"User created Successfully", data:{username, email, password}});
-    } catch (error) {
-        res.status(401).json({message:"Error creating user"});
-    }
 }
 
 export default signup;
